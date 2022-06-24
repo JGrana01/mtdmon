9ef430c9f501928d4f1bff2f8810d896a602036d
John Grana <jgrana@rochester.rr.com>
2022-06-24 17:46:04 -0400

    V0.6.2 - still debugging upgrade

--- mtdmon.sh
+++ mtdmon.sh
@@ -28,7 +28,7 @@
 
 ### Start of script variables ###
 readonly SCRIPT_NAME="mtdmon"
-readonly SCRIPT_VERSION="v0.6.0"
+readonly SCRIPT_VERSION="v0.6.2"
 SCRIPT_BRANCH="main"
 MTDAPP_BRANCH="main"
 SCRIPT_REPO="https://raw.githubusercontent.com/JGrana01/mtdmon/$SCRIPT_BRANCH"
